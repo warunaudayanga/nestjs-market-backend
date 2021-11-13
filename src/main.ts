717411c6caea7ab80db5bc1e2490425e971fac30
Waruna Udayanga <avawaruna@gmail.com>
2021-11-13 20:04:31 +0530

    * Fix main

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from "@nestjs/common";
 import { AllExceptionsFilter } from "./common/filters/http-exception.filter";
 import { LoggerService } from "./common/services/logger.service";
 import { AppModule } from "./app.module";
-import * as helmet from "helmet";
+// import * as helmet from "helmet";
 import * as cookieParser from "cookie-parser";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type,func-style
@@ -15,7 +15,7 @@ async function bootstrap() {
     const { httpAdapter } = app.get(HttpAdapterHost);
     app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
     app.use(cookieParser());
-    app.use(helmet());
+    // app.use(helmet());
     app.enableCors();
     // app.setViewEngine("");
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
