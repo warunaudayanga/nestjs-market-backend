2a684831cba6dd7c7c6151a0a982fc95577c30c9
Waruna Udayanga <avawaruna@gmail.com>
2021-11-13 20:13:02 +0530

    * Fix main

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,14 @@ import * as cookieParser from "cookie-parser";
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type,func-style
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        cors: true,
         logger: new LoggerService()
     });
     const { httpAdapter } = app.get(HttpAdapterHost);
     app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
     app.use(cookieParser());
     // app.use(helmet());
-    app.enableCors();
+    // app.enableCors();
     // app.setViewEngine("");
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     app.setGlobalPrefix("api");
