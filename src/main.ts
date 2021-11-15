750601af432bff4b26a36de8254616d0eae94a6c
Waruna Udayanga <avawaruna@gmail.com>
2021-11-15 10:07:58 +0530

    * ix Dyno

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,17 @@ import { ValidationPipe } from "@nestjs/common";
 import { AllExceptionsFilter } from "./common/filters/http-exception.filter";
 import { LoggerService } from "./common/services/logger.service";
 import { AppModule } from "./app.module";
-// import * as helmet from "helmet";
+import * as helmet from "helmet";
 import * as cookieParser from "cookie-parser";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type,func-style
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-        cors: true,
-        logger: new LoggerService()
-    });
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, { logger: new LoggerService() });
     const { httpAdapter } = app.get(HttpAdapterHost);
     app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
     app.use(cookieParser());
-    // app.use(helmet());
-    // app.enableCors();
+    app.use(helmet());
+    app.enableCors();
     // app.setViewEngine("");
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     app.setGlobalPrefix("api");
